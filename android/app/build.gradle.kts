f1581c1b8c965d855f9423a7668d155bc73d8db8
Marvin Baudach <tomateq@pm.me>
2026-08-15 21:15:28 +0200

    chore: bump version to 0.1.12

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "org.reprise"
         minSdk = 26
         targetSdk = 37
-        versionCode = 11
+        versionCode = 12
         versionName = workspacePackageValue("version")
         buildConfigField("String", "REPRISE_CORE_VERSION", "\"${workspacePackageValue("version")}\"")
         buildConfigField("String", "REPRISE_CORE_LICENSE", "\"${workspacePackageValue("license")}\"")
