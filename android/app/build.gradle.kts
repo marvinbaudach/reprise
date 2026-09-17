c26e4b079dff25525280f5e19869a57dd7777938
Marvin Baudach <tomateq@pm.me>
2026-09-17 04:44:30 +0200

    chore: bump version to desktop 0.1.211, android 0.1.143

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "io.github.marvinbaudach.reprise"
         minSdk = 26
         targetSdk = 37
-        versionCode = 142
-        versionName = "0.1.142"
+        versionCode = 143
+        versionName = "0.1.143"
         buildConfigField("String", "REPRISE_CORE_VERSION", "\"${workspacePackageValue("version")}\"")
         buildConfigField("String", "REPRISE_CORE_LICENSE", "\"${workspacePackageValue("license")}\"")
         buildConfigField("String", "REPRISE_MOBILE_LICENSE", "\"GPL-3.0-or-later\"")
