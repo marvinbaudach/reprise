dbbc9c72a400097c5a6f792daea3c2094c1a1572
Marvin Baudach <tomateq@pm.me>
2026-09-24 10:05:29 +0300

    Raise desktop to 0.1.219 and Android to 0.1.159

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "io.github.marvinbaudach.reprise"
         minSdk = 26
         targetSdk = 37
-        versionCode = 158
-        versionName = "0.1.158"
+        versionCode = 159
+        versionName = "0.1.159"
         buildConfigField("String", "REPRISE_CORE_VERSION", "\"${workspacePackageValue("version")}\"")
         buildConfigField("String", "REPRISE_CORE_LICENSE", "\"${workspacePackageValue("license")}\"")
         buildConfigField("String", "REPRISE_MOBILE_LICENSE", "\"GPL-3.0-or-later\"")
